d6c626478704cfe64f8e1818f6eeb5ce13f571d1
MichellMurallas <michellsms893@gmail.com>
2023-04-07 19:40:00 -0500

    sessio-with-token

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import  express  from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
 import counterRouter from "./routes/account.js";
 import authRouter from "./routes/auth.js";
+import authTokenRouter from "./routes/auth_token.js";
+import authSessionRouter from "./routes/auth_session.js";
+
 
 
 dotenv.config();
@@ -10,10 +14,15 @@ console.clear()
 const PORT = process.env.PORT || 3000;
 const expressApp = express();
 
+
+expressApp.use(cookieParser())
 expressApp.use(express.json())
 expressApp.use(express.text())
 expressApp.use("/account", counterRouter);
 expressApp.use("/auth", authRouter);
+
+expressApp.use("/auth-token", authTokenRouter);
+expressApp.use("/auth-session", authSessionRouter);
 
 expressApp.listen(PORT, ()=>
   console.log(`Servidor levantado en el puerto ${PORT}`)
